d727b4fcf9f08e2d5e8de5f4cecb929c20d60329
Gabiru <gabrielsampaiobicalho@gmail.com>
2023-01-08 14:56:13 -0300

    feat(Header) estiliza header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,20 @@
+import { HeaderContainer } from './Header.styles'
+import { Timer, Scroll } from 'phosphor-react'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { NavLink } from 'react-router-dom'
+
 export default function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="Dois triangulos verder um por cima do outro" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
