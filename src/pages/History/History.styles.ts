be644bede54ca272be9db8a40d2126ecdd377d91
Gabiru <gabrielsampaiobicalho@gmail.com>
2023-01-15 21:12:45 -0300

    feat(contexto) implementa reducer no contexto

--- src/pages/History/History.styles.ts
+++ src/pages/History/History.styles.ts
@@ -43,7 +43,7 @@ export const HistoryList = styled.main`
 
     td {
       background-color: ${(props) => props.theme['gray-700']};
-      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      border-top: 4px solid ${(props) => props.theme['gray-100']};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
